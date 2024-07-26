3f4ebf03d34b1497d720ce82f778001158ec7595
robertoveron <106416225+robertoveron@users.noreply.github.com>
2024-07-25 23:04:55 -0400

    new commit

--- src/main/java/variousConcepts/GitTest2.java
+++ src/main/java/variousConcepts/GitTest2.java
@@ -1,5 +1,20 @@
 package variousConcepts;
 
 public class GitTest2 {
+	
+	public static void main(String[] args) {
+		System.out.println("git test");
+
+	}
+    public void printme() {
+    	System.out.println("git test1");
+    	System.out.println("git test1,2");
+    	System.out.println("git test1,2,3");
+    
+    
+    }
+    
+    
+
 
 }
